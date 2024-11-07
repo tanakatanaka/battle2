9c0c04470932fced9a5dbc14ef881df85cf4d04b
tanaka-orenda <tanaka_t@orenda.co.jp>
2024-11-07 17:21:09 +0900

    構造体を取得する関数を追加

--- Source/battle_/UI/InventoryUserWidget.cpp
+++ Source/battle_/UI/InventoryUserWidget.cpp
@@ -2,9 +2,37 @@
 
 
 #include "InventoryUserWidget.h"
+#include "../Struct/ItemParamStruct.h"
 #include "Engine/DataTable.h"
 
 void UInventoryUserWidget::ReadStruct()
 {
+    // Data Tableアセットを取得
+    static ConstructorHelpers::FObjectFinder<UDataTable> DataTableObj(TEXT("DataTable'/Game/StylishCombatKit/Struct/DataTable/DT_ItemParaDB'"));
+    
+    if (DataTableObj.Succeeded())
+    {
+        UDataTable* DataTable = DataTableObj.Object;
+
+        for (const auto& Row : DataTable->GetRowMap())
+        {
+            FItemParamStruct* DataRow = (FItemParamStruct*)(Row.Value);
+
+            if (DataRow)
+            {
+                /*
+                // 各プロパティにアクセス
+                UE_LOG(LogTemp, Log, TEXT("Name: %s, Age: %d, Score: %f"),
+                    *DataRow->Name,
+                    DataRow->Age,
+                    DataRow->Score);
+                */
+            }
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to load DataTable"));
+    }
     
 }
