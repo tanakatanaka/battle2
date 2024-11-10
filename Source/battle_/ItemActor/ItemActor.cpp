7bab32e73a941545f72aa4882678383c5a0ce7af
tanatana <aiueo2003haru@gmail.com>
2024-11-10 19:59:32 +0900

    不要関数を除去

--- Source/battle_/ItemActor/ItemActor.cpp
+++ Source/battle_/ItemActor/ItemActor.cpp
@@ -4,26 +4,5 @@
 #include "ItemActor.h"
 #include "Kismet/GameplayStatics.h"
 
-// Sets default values
-AItemActor::AItemActor()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AItemActor::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void AItemActor::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
 
 
--- enc_temp_folder/5b8d1c2258bde6fdd9557848189e11b/ItemActor.h
+++ enc_temp_folder/5b8d1c2258bde6fdd9557848189e11b/ItemActor.h
@@ -13,6 +13,17 @@ class BATTLE__API AItemActor : public AActor
 	GENERATED_BODY()
 	
 public:	
+	// Sets default values for this actor's properties
+	AItemActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ItemParam")
 	FItemParamStruct ItemParam;
 
