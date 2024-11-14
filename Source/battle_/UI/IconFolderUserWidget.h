477ce5da43f4e5a919165e7f769ac48657370299
tanaka-orenda <tanaka_t@orenda.co.jp>
2024-11-14 13:30:46 +0900

    iconを取得する関数を追加

---
+++ Source/battle_/UI/IconFolderUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "IconFolderUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLE__API UIconFolderUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
