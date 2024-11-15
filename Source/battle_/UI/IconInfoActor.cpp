3c60e6170799cd9fa1da633c81ebe59fe6e39d7b
tanaka-orenda <tanaka_t@orenda.co.jp>
2024-11-15 18:59:37 +0900

    iconの内部に潜らせるクラスを作成

---
+++ Source/battle_/UI/IconInfoActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IconInfoActor.h"
+
+// Sets default values
+AIconInfoActor::AIconInfoActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AIconInfoActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AIconInfoActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
